2dd3624b5301e5ab69c59fb8bc865d231f9a02cb
quexinyan <hzddxf0035>
2018-12-05 16:30:37 +0800

    Initial commit

---
+++ service-feign/src/main/java/com/quexinyan/servicefeign/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.quexinyan.servicefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务。比如在代码中调用了service-hi服务的“/hi”接口
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-feign/src/main/java/com/quexinyan/servicefeign/ServiceFeignApplication.java
@@ -0,0 +1,19 @@
+package com.quexinyan.servicefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ *启动类ServiceFeignApplication ，加上@EnableFeignClients注解开启Feign的功能
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServiceFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFeignApplication.class, args);
+	}
+}
