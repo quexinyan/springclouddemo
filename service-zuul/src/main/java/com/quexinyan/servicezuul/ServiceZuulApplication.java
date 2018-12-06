d33ce612080745044efe1deb25a0764048598d9c
quexinyan <hzddxf0035>
2018-12-06 17:40:35 +0800

    zuul

--- service-zuul/src/main/java/com/quexinyan/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/quexinyan/servicezuul/ServiceZuulApplication.java
@@ -2,8 +2,17 @@ package com.quexinyan.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * applicaton类加上注解@EnableZuulProxy，开启zuul的功能
+ */
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceZuulApplication {
 
 	public static void main(String[] args) {
